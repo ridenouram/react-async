0fefa2a7c3167b3ece73f424f7e42136b019c891
Anna Ridenour <annamridenour@gmail.com>
2019-05-17 14:18:13 -0700

    created CharacterQuotes functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import TopQuotes from './TopQuotes';
+import CharacterQuotes from './CharacterQuotes';
 
 export default function App() {
   return (
-    <TopQuotes count={10} />
+    
+    // <TopQuotes count={10} />
+    <CharacterQuotes count={10} character='Leela' />
   );
 }
